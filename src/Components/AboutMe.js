c9ac9d1901b2653027007d2d64873b210d1e5122
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-07-14 20:20:09 -0700

    commit -m "First Commit. Set general outline for the project"

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function App() {
+export default function AboutMe() {
   return (
     <div>
-      <h2>app.js going in</h2>
+      
     </div>
   )
 }
--- src/Components/Github.js
+++ src/Components/Github.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function App() {
+export default function Github() {
   return (
     <div>
-      <h2>app.js going in</h2>
+      
     </div>
   )
 }
